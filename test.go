f2287aeedd0eed5fca30ab2d0b8e4705c6738df5
justyouso <1242646507@qq.com>
2020-08-25 23:32:35 +0800

    feat(熔断器): 熔断器的基础使用

--- test.go
+++ test.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/afex/hystrix-go/hystrix"
 	"golang.org/x/time/rate"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -47,14 +49,108 @@ func MyLimit(next http.Handler) http.Handler {
 	})
 }
 
+type Product struct {
+	ID    int
+	Title string
+	Price int
+}
+
+func getProduct() (Product, error) {
+	r := rand.Intn(10)
+	if r < 6 {
+		time.Sleep(time.Second * 5)
+	}
+	return Product{
+		ID:    101,
+		Title: "Golang",
+		Price: 10,
+	}, nil
+}
+
+func resProduct() (Product, error) {
+	return Product{
+		ID:    100,
+		Title: "推荐商品",
+		Price: 100,
+	}, nil
+}
+
 func main() {
 	//阻塞等待
 	//waitn()
 	//allow()
 	//限流模式
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("OK!!!"))
-	})
-	http.ListenAndServe(":8080", MyLimit(mux))
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	writer.Write([]byte("OK!!!"))
+	//})
+	//http.ListenAndServe(":8080", MyLimit(mux))
+	rand.Seed(time.Now().UnixNano())
+
+	configA := hystrix.CommandConfig{
+		Timeout:                2000,
+		MaxConcurrentRequests:  5,
+		RequestVolumeThreshold: 4,
+		ErrorPercentThreshold:  50,
+		SleepWindow:            10,
+	}
+	hystrix.ConfigureCommand("get_prod", configA)
+
+	//hystrix.ConfigureCommand("get_prod2",hystrix.CommandConfig{
+	//	Timeout: 2000,
+	//})
+
+	//resultChan := make(chan Product, 1)
+	//wg := sync.WaitGroup{}
+
+	////使用协程模拟并发
+	//for i := 0; i < 10; i++ {
+	//	go (func() {
+	//		wg.Add(1)
+	//		defer wg.Done()
+	//		for {
+	//			errs := hystrix.Go("get_prod", func() error {
+	//				p, _ := getProduct()
+	//				resultChan <- p
+	//				return nil
+	//			}, func(e error) error {
+	//				fmt.Println(e)
+	//				rec, err := resProduct()
+	//				resultChan <- rec
+	//				//fmt.Println(rec)
+	//				//fmt.Println(resProduct())
+	//				return err
+	//			})
+	//			select {
+	//			case getProd := <-resultChan:
+	//				fmt.Println(getProd)
+	//			case err := <-errs:
+	//				fmt.Println(err)
+	//			}
+	//			time.Sleep(time.Second * 1)
+	//		}
+	//	})()
+	//}
+	//wg.Wait()
+
+	//取出熔断器的状态
+	c, _, _ := hystrix.GetCircuit("get_prod")
+	for i := 0; i < 100; i++ {
+		errs := hystrix.Do("get_prod", func() error {
+			p, _ := getProduct()
+			fmt.Println(p)
+			return nil
+		}, func(e error) error {
+			//fmt.Println(e)
+			rcp, err := resProduct()
+			fmt.Println(rcp)
+			return err
+		})
+		if errs != nil {
+			//fmt.Println(errs)
+		}
+		fmt.Println(c.IsOpen())
+		time.Sleep(time.Second * 1)
+	}
+
 }
